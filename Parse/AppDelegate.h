d8351f4167a0a3fc380c422c19d4e82ed403bfc9
taomin <itiachang@gmail.com>
2014-09-25 19:54:25 -0700

    Initial Commit

---
+++ Parse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Parse
+//
+//  Created by Taomin Chang on 9/25/14.
+//  Copyright (c) 2014 Taomin Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Parse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Parse
+//
+//  Created by Taomin Chang on 9/25/14.
+//  Copyright (c) 2014 Taomin Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
